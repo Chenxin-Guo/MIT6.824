0882b3fac7cd95041c91c13b9a85dca6d371f702
Chenxin-Guo <43798997+Chenxin-Guo@users.noreply.github.com>
2021-01-01 15:09:31 -0800

    complete task manage functions

--- src/mr/task_manager.go
+++ src/mr/task_manager.go
@@ -1,6 +1,10 @@
 package mr
 
-import "time"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 // Task represents the status of a single map/reduce task.
 type Task struct {
@@ -16,30 +20,70 @@ type Task struct {
 
 // TaskManager manages unfinished map/reduce tasks
 type TaskManager struct {
-	Tasks map[int]Task
+	Tasks map[int]*Task
+
+	mu sync.Mutex
 }
 
 // AddMapTask adds a new map task to the task manager.
-func (t *TaskManager) AddMapTask(index int, mapFile string) {
-
+func (t *TaskManager) AddMapTask(index int, mapFile string) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	_, ok := t.Tasks[index]
+	if ok {
+		return fmt.Errorf("Index %v is already added", index)
+	}
+	t.Tasks[index] = &Task{
+		IsMapTask:  true,
+		IsAssigned: false,
+		FilePath:   mapFile,
+	}
+	return nil
 }
 
-// AddReduceTask adds a new map task to the task manager.
-func (t *TaskManager) AddReduceTask(index int, mapFile string) {
-
+// AddReduceTask adds a new reduce task to the task manager.
+func (t *TaskManager) AddReduceTask(index int) error {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	if _, ok := t.Tasks[index]; ok {
+		return fmt.Errorf("Index %v is already added", index)
+	}
+	t.Tasks[index] = &Task{
+		IsMapTask:  false,
+		IsAssigned: false,
+	}
+	return nil
 }
 
 // GetAssignment returns a pending task.
-func (t *TaskManager) GetAssignment() Task {
-
+func (t *TaskManager) GetAssignment() *Task {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	for _, v := range t.Tasks {
+		if !v.IsAssigned || time.Now().Sub(v.AssignTime) > 10*time.Second {
+			v.AssignTime = time.Now()
+			v.IsAssigned = true
+			return v
+		}
+	}
+	return nil
 }
 
 // RemoveTask removes a task from the task manager.
-func (t *TaskManager) RemoveTask(index int) Task {
-
+func (t *TaskManager) RemoveTask(index int) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	if _, ok := t.Tasks[index]; ok {
+		delete(t.Tasks, index)
+	}
 }
 
 // Done returns true if there is no more pending task.
 func (t *TaskManager) Done() bool {
-
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	if len(t.Tasks) == 0 {
+		return true
+	}
+	return false
 }

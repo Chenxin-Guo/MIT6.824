02f266a325e1d0b1baad282ac23a9a4d4c565c8a
Dajun_Luo <luodajun100@gmail.com>
2021-01-01 15:13:29 -0800

    Add protocol definition

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -22,8 +24,46 @@ type ExampleReply struct {
 	Y int
 }
 
-// Add your RPC definitions here.
+// RequestTaskArgs is the Request Message for RequestTask
+type RequestTaskArgs struct{}
 
+// RequestTaskReply is the Reply Message for RequestTask
+type RequestTaskReply struct {
+	// True if there is a task assigned.
+	HasTask bool
+	// True if the task is a map task, otherwise it is a reduce task.
+	IsMapTask bool
+	// The index of the task.
+	index int
+	// Number of mapper of this MapReduce Job.
+	NMapper int
+	// Number of reducer of this MapReduce Job.
+	NReducer int
+	// The paht of the file to be mapped. Only vaid if IsMapTask is True.
+	MapFile string
+}
+
+// SubmitTaskArgs is the Request Message for SubmitTask
+type SubmitTaskArgs struct {
+	// True if the task to be submitted is a map task, otherwise is a reduce task.
+	IsMapTask bool
+	// The index of the task.
+	index int
+}
+
+// SubmitTaskReply is the Reply Message for SubmitTask
+type SubmitTaskReply struct{}
+
+// DoneArgs is the Request Message for QueryDone
+type DoneArgs struct{}
+
+// DoneReply is the Reply Message for QueryDone
+type DoneReply struct {
+	// True if the map reduce job is done. Woker should exit upon seeing true.
+	IsDone bool
+}
+
+// Add your RPC definitions here.
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the master.
